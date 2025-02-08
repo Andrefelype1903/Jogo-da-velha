2645e06adfcb5bb4b312dd5fc93073258dce8419
andrefelype <andrefilype21122014@gmail.com>
2025-02-08 20:14:11 -0300

    terminando responsividade

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const Campo7 = document.querySelector('#campo7')
 const Campo8 = document.querySelector('#campo8')
 const Campo9 = document.querySelector('#campo9')
 
-const ElementoMain = document.querySelector('.conteiner')
+const ElementoMain = document.querySelector('.corpo')
 
 const rodape = document.querySelector('.rodape');
 
@@ -23,24 +23,6 @@ const CorrigeCanvas = () => {
     LinhaVenceu.width = ElementoMain.offsetWidth;
     LinhaVenceu.height = ElementoMain.offsetHeight;
 }
-
-/* const LinhaVencedorHorizontal = (x,y,z) => {
-    const LinhaVenceu = document.createElement('canvas');
-    LinhaVenceu.classList.add('canvas');
-    document.querySelector('.rodape');
-    document.body.insertBefore(LinhaVenceu,rodape)
-     let ctx = LinhaVenceu.getContext('2d');
-     ctx.beginPath();
-     ctx.moveTo(x,y);
-     ctx.lineTo(z,y);
-     if(vez === 1) {
-         ctx.strokeStyle = 'red'
-     } else if(vez  === 0) {
-        ctx.strokeStyle = 'blue'
-     }
-     ctx.stroke()
-
-} */
 
 const LinhaVencedorHorizontal = (x,y,z) => {
     const LinhaVenceu = document.createElement('canvas');
@@ -64,25 +46,6 @@ const LinhaVencedorHorizontal = (x,y,z) => {
 
 }
 
-/* const LinhaVencedorVertical = (x,y,z) => {
-    const LinhaVenceu = document.createElement('canvas');
-    LinhaVenceu.classList.add('canvas');
-    document.querySelector('.rodape');
-    document.body.insertBefore(LinhaVenceu,rodape)
-     let ctx = LinhaVenceu.getContext('2d');
-     ctx.beginPath();
-     ctx.moveTo(x,y);
-     ctx.lineTo(x,z);
-     if(vez === 1) {
-        ctx.strokeStyle = 'red'
-    } else if(vez  === 0) {
-       ctx.strokeStyle = 'blue'
-    }
-     ctx.stroke()
-
-} */
-
-
 const LinhaVencedorVertical = (x,y,z) => {
     const LinhaVenceu = document.createElement('canvas');
     LinhaVenceu.classList.add('canvas');
@@ -105,25 +68,6 @@ const LinhaVencedorVertical = (x,y,z) => {
 
 }
 
-/* const LinhaVencedorDiagonal = (x,y,z,w) => {
-    const LinhaVenceu = document.createElement('canvas');
-    LinhaVenceu.classList.add('canvas');
-    document.querySelector('.rodape');
-    document.body.insertBefore(LinhaVenceu,rodape)
-     let ctx = LinhaVenceu.getContext('2d');
-     ctx.beginPath();
-     ctx.moveTo(x,y);
-     ctx.lineTo(z,w);
-     if(vez === 1) {
-        ctx.strokeStyle = 'red'
-    } else if(vez  === 0) {
-       ctx.strokeStyle = 'blue'
-    }
-     ctx.stroke()
-
-} */
-
-
 const LinhaVencedorDiagonal = (x,y,z,w) => {
     const LinhaVenceu = document.createElement('canvas');
     LinhaVenceu.classList.add('canvas');
@@ -142,8 +86,9 @@ const LinhaVencedorDiagonal = (x,y,z,w) => {
     }
      ctx.lineWidth = ExpessuraLinha;
      ctx.stroke()
-
 }
+
+/* tester posiçoes da linha de vencedor */
 
 /* LinhaVencedorHorizontal(0,0.16,1)
 LinhaVencedorHorizontal(0,0.5,1)
@@ -173,9 +118,6 @@ const TelaVencedor = (v) => {
 }
 
 
-
-
-
 Campo1.addEventListener('click', () => {
     if(vez === 0){
         Campo1.textContent = x
@@ -186,29 +128,28 @@ Campo1.addEventListener('click', () => {
         Campo1.style.color = 'blue'
         vez = vez - 1
     }
-
-    if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent || Campo1.textContent === Campo4.textContent && Campo7.textContent === Campo1.textContent || Campo1.textContent === Campo5.textContent && Campo9.textContent === Campo1.textContent) {
-       setTimeout(() => {
-
-        TelaVencedor(Campo1.textContent);
         
-        if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent) {
+    if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent) {
 
-            LinhaVencedorHorizontal(0,0.16,1)
+        setTimeout(() => {
+            LinhaVencedorHorizontal(0,0.16,1);
+            TelaVencedor(Campo1.textContent);
+        },100)
 
-        } else if(Campo1.textContent === Campo4.textContent && Campo1.textContent === Campo7.textContent) {
+    } else if(Campo1.textContent === Campo4.textContent && Campo1.textContent === Campo7.textContent) {
 
-            LinhaVencedorVertical(0.161,0,1)
+        setTimeout(() => {
+            LinhaVencedorVertical(0.161,0,1);
+            TelaVencedor(Campo1.textContent);
+        },100)
 
-        } else if(Campo1.textContent === Campo5.textContent && Campo9.textContent === Campo1.textContent) {
+    } else if(Campo1.textContent === Campo5.textContent && Campo9.textContent === Campo1.textContent) {
 
-            LinhaVencedorDiagonal(0,0,1,1)
-
-        }
-
-       },100)
+        setTimeout(() => {
+            LinhaVencedorDiagonal(0,0,1,1);
+            TelaVencedor(Campo1.textContent);
+        })
     }
-    
     Campo1.style.pointerEvents = "none"
 })
 
@@ -221,25 +162,22 @@ Campo2.addEventListener('click', () => {
         Campo2.textContent = o
         Campo2.style.color = 'blue'
         vez = vez - 1
-    }
+    }     
 
-    if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent || Campo2.textContent === Campo5.textContent && Campo8.textContent === Campo2.textContent) {
+    if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent) {
+
         setTimeout(() => {
-
+            LinhaVencedorHorizontal(0,0.16,1);
             TelaVencedor(Campo2.textContent);
-
-            if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent) {
-
-                LinhaVencedorHorizontal(0,0.16,1)
-
-            } else if(Campo2.textContent === Campo5.textContent && Campo8.textContent === Campo2.textContent) {
-
-                LinhaVencedorVertical(0.5,0,1)
-
-            }
-
         },100)
-     }
+
+    } else if(Campo2.textContent === Campo5.textContent && Campo8.textContent === Campo2.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorVertical(0.5,0,1);
+            TelaVencedor(Campo2.textContent);
+        },100)
+    }
 
     Campo2.style.pointerEvents = "none"
 })
@@ -254,27 +192,28 @@ Campo3.addEventListener('click', () => {
         Campo3.style.color = 'blue'
         vez = vez - 1
     }
+            
+    if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent) {
 
-    if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent || Campo3.textContent === Campo6.textContent && Campo9.textContent === Campo3.textContent || Campo3.textContent === Campo5.textContent && Campo7.textContent === Campo3.textContent) {
         setTimeout(() => {
-
+            LinhaVencedorHorizontal(0,0.16,1);
             TelaVencedor(Campo3.textContent);
-    
-            if(Campo1.textContent === Campo2.textContent && Campo3.textContent === Campo1.textContent) {
-
-                LinhaVencedorHorizontal(0,0.16,1)
-
-            } else if(Campo3.textContent === Campo6.textContent && Campo9.textContent === Campo3.textContent) {
-
-                LinhaVencedorVertical(0.8389,0,1)
-
-            } else if(Campo3.textContent === Campo5.textContent && Campo7.textContent === Campo3.textContent) {
-
-                LinhaVencedorDiagonal(1,0,0,1)
-
-            }
         },100)
-     }
+
+    } else if(Campo3.textContent === Campo6.textContent && Campo9.textContent === Campo3.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorVertical(0.8389,0,1);
+            TelaVencedor(Campo3.textContent);
+        },100)
+
+    } else if(Campo3.textContent === Campo5.textContent && Campo7.textContent === Campo3.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorDiagonal(1,0,0,1);
+            TelaVencedor(Campo3.textContent);
+        },100)
+    }
 
     Campo3.style.pointerEvents = "none"
 })
@@ -289,23 +228,21 @@ Campo4.addEventListener('click', () => {
         Campo4.style.color = 'blue'
         vez = vez - 1
     }
-    if(Campo4.textContent === Campo5.textContent && Campo4.textContent === Campo6.textContent || Campo4.textContent === Campo1.textContent && Campo4.textContent === Campo7.textContent) {
+    
+    if(Campo4.textContent === Campo5.textContent && Campo4.textContent === Campo6.textContent) {
+
         setTimeout(() => {
-
+            LinhaVencedorHorizontal(0,0.5,1);
             TelaVencedor(Campo4.textContent)
-         
-            if(Campo4.textContent === Campo5.textContent && Campo4.textContent === Campo6.textContent) {
-
-                LinhaVencedorHorizontal(0,0.5,1)
-
-            } else if(Campo4.textContent === Campo1.textContent && Campo4.textContent === Campo7.textContent) {
-
-                LinhaVencedorVertical(0.161,0,1)
-
-            }
-
         },100)
-     }
+
+    } else if(Campo4.textContent === Campo1.textContent && Campo4.textContent === Campo7.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorVertical(0.161,0,1);
+            TelaVencedor(Campo4.textContent)
+        },100)
+    }
 
     Campo4.style.pointerEvents = "none"
 })
@@ -321,31 +258,34 @@ Campo5.addEventListener('click', () => {
         vez = vez - 1;
     }
 
-    if(Campo4.textContent === Campo5.textContent && Campo5.textContent === Campo6.textContent || Campo2.textContent === Campo5.textContent && Campo2.textContent === Campo8.textContent || Campo1.textContent === Campo5.textContent && Campo1.textContent === Campo9.textContent || Campo3.textContent === Campo5.textContent && Campo3.textContent === Campo7.textContent) {
+    if(Campo4.textContent === Campo5.textContent && Campo5.textContent === Campo6.textContent) {
+
         setTimeout(() => {
-
-            TelaVencedor(Campo5.textContent)
-
-            if(Campo4.textContent === Campo5.textContent && Campo5.textContent === Campo6.textContent) {
-
-                LinhaVencedorHorizontal(0,0.5,1)
-  
-            } else if(Campo2.textContent === Campo5.textContent && Campo2.textContent === Campo8.textContent ){
-
-                LinhaVencedorVertical(0.5,0,1)
-
-            } else if(Campo1.textContent === Campo5.textContent && Campo1.textContent === Campo9.textContent) {
-
-                LinhaVencedorDiagonal(0,0,1,1)
-
-            } else if(Campo3.textContent === Campo5.textContent && Campo3.textContent === Campo7.textContent) {
-
-                LinhaVencedorDiagonal(1,0,0,1)
-
-            }
-
+            LinhaVencedorHorizontal(0,0.5,1);
+            TelaVencedor(Campo5.textContent);
         },100)
-     }
+
+    } else if(Campo2.textContent === Campo5.textContent && Campo2.textContent === Campo8.textContent ){
+
+        setTimeout(() => {
+            LinhaVencedorVertical(0.5,0,1);
+            TelaVencedor(Campo5.textContent);
+        },100)
+
+    } else if(Campo1.textContent === Campo5.textContent && Campo1.textContent === Campo9.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorDiagonal(0,0,1,1);
+            TelaVencedor(Campo5.textContent);
+        },100)
+
+    } else if(Campo3.textContent === Campo5.textContent && Campo3.textContent === Campo7.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorDiagonal(1,0,0,1);
+            TelaVencedor(Campo5.textContent);          
+        },100)
+    }
 
     Campo5.style.pointerEvents = "none"
 })
@@ -361,23 +301,20 @@ Campo6.addEventListener('click', () => {
         vez = vez - 1
     }
 
-    if(Campo4.textContent === Campo5.textContent && Campo4.textContent === Campo6.textContent || Campo3.textContent === Campo6.textContent && Campo3.textContent === Campo9.textContent) {
+    if(Campo4.textContent === Campo5.textContent && Campo4.textContent === Campo6.textContent) {
+
         setTimeout(() => {
-         
-            TelaVencedor(Campo6.textContent)
-
-            if(Campo4.textContent === Campo5.textContent && Campo4.textContent === Campo6.textContent) {
-
-                LinhaVencedorHorizontal(0,0.5,1)
-
-            } else if(Campo3.textContent === Campo6.textContent && Campo3.textContent === Campo9.textContent) {
-
-                LinhaVencedorVertical(0.8389,0,1);
-
-            }
-
+            LinhaVencedorHorizontal(0,0.5,1);
+            TelaVencedor(Campo6.textContent);
         },100)
-     }
+
+    } else if(Campo3.textContent === Campo6.textContent && Campo3.textContent === Campo9.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorVertical(0.8389,0,1);
+            TelaVencedor(Campo6.textContent);   
+        },100)
+    }
 
     Campo6.style.pointerEvents = "none"
 })
@@ -393,27 +330,26 @@ Campo7.addEventListener('click', () => {
         vez = vez - 1
     }
 
-    if(Campo1.textContent === Campo4.textContent && Campo1.textContent === Campo7.textContent || Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent || Campo7.textContent === Campo5.textContent && Campo7.textContent === Campo3.textContent) {
+    if(Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent) {
+
         setTimeout(() => {
-         
-            TelaVencedor(Campo7.textContent)
-
-            if(Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent) {
-
-                LinhaVencedorHorizontal(0,0.84,1)
-
-            } else if(Campo1.textContent === Campo4.textContent && Campo1.textContent === Campo7.textContent) {
-
-                LinhaVencedorVertical(0.161,0,1);
-
-            } else if(Campo7.textContent === Campo5.textContent && Campo7.textContent === Campo3.textContent) {
-
-                LinhaVencedorDiagonal(1,0,0,1);
-
-            }
-
+            LinhaVencedorHorizontal(0,0.84,1);
+            TelaVencedor(Campo7.textContent);    
         },100)
-     }
+
+    } else if(Campo1.textContent === Campo4.textContent && Campo1.textContent === Campo7.textContent) {
+        setTimeout(() => {
+            LinhaVencedorVertical(0.161,0,1);
+            TelaVencedor(Campo7.textContent); 
+        },100)
+
+    } else if(Campo7.textContent === Campo5.textContent && Campo7.textContent === Campo3.textContent) {
+        setTimeout(() => {
+            LinhaVencedorDiagonal(1,0,0,1);
+            TelaVencedor(Campo7.textContent);    
+        },100)
+
+    }
 
     Campo7.style.pointerEvents = "none"
 })
@@ -427,25 +363,22 @@ Campo8.addEventListener('click', () => {
         Campo8.textContent = o;
         Campo8.style.color = 'blue'
         vez = vez - 1
-    }
+    }  
 
-    if(Campo2.textContent === Campo5.textContent && Campo2.textContent === Campo8.textContent || Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent) {
+    if(Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent) {
+
         setTimeout(() => {
-         
+            LinhaVencedorHorizontal(0,0.84,1);
             TelaVencedor(Campo8.textContent);
-
-            if(Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent) {
-
-                LinhaVencedorHorizontal(0,0.84,1)
-
-            } else if(Campo2.textContent === Campo5.textContent && Campo2.textContent === Campo8.textContent) {
-
-                LinhaVencedorVertical(0.5,0,1)
-
-            }
-
         },100)
-     }
+
+    } else if(Campo2.textContent === Campo5.textContent && Campo2.textContent === Campo8.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorVertical(0.5,0,1);
+            TelaVencedor(Campo8.textContent);
+        },100)
+    }
 
     Campo8.style.pointerEvents = "none"
 })
@@ -459,33 +392,32 @@ Campo9.addEventListener('click', () => {
         Campo9.textContent = o;
         Campo9.style.color = 'blue'
         vez = vez - 1
-    }
+    }      
 
-    if(Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent || Campo3.textContent === Campo6.textContent && Campo3.textContent === Campo9.textContent || Campo1.textContent === Campo5.textContent && Campo1.textContent === Campo9.textContent) {
+    if(Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent) {
+
         setTimeout(() => {
-        
+            LinhaVencedorHorizontal(0,0.84,1);
             TelaVencedor(Campo9.textContent);
-
-            if(Campo7.textContent === Campo8.textContent && Campo7.textContent === Campo9.textContent) {
-
-                LinhaVencedorHorizontal(0,0.84,1)
-
-            } else if( Campo3.textContent === Campo6.textContent && Campo3.textContent === Campo9.textContent) {
-
-                LinhaVencedorVertical(0.8389,0,1);
-
-            } else if(Campo1.textContent === Campo5.textContent && Campo1.textContent === Campo9.textContent) {
-
-                LinhaVencedorDiagonal(0,0,1,1)
-
-            }
-
         },100)
-     }
+
+    } else if( Campo3.textContent === Campo6.textContent && Campo3.textContent === Campo9.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorVertical(0.8389,0,1);
+            TelaVencedor(Campo9.textContent);
+        },100)
+
+    } else if(Campo1.textContent === Campo5.textContent && Campo1.textContent === Campo9.textContent) {
+
+        setTimeout(() => {
+            LinhaVencedorDiagonal(0,0,1,1);
+            TelaVencedor(Campo9.textContent);
+        },100)
+    }
 
     Campo9.style.pointerEvents = "none"
 })
 
-window.addEventListener('resize', CorrigeCanvas)
+// window.addEventListener('resize', CorrigeCanvas)
 
-// tornar o código responsivo
